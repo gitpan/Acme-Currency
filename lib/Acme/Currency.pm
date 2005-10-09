8bea8f22d152d7e96db59bb01487d1ef1abdfd3a
Steffen Müller <smueller@cpan.org>
2005-10-09 10:08:28 +0000

    Import of SMUELLER/Acme-Currency-1.00 from CPAN.
    gitpan-cpan-distribution: Acme-Currency
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         SMUELLER/Acme-Currency-1.00.tar.gz
    gitpan-cpan-author:       SMUELLER
    gitpan-cpan-maturity:     released

--- lib/Acme/Currency.pm
+++ lib/Acme/Currency.pm
@@ -6,7 +6,7 @@ use strict;
 use warnings;
 
 use vars '$VERSION';
-$VERSION = '0.01';
+$VERSION = '1.00';
 
 use Filter::Simple;
 
@@ -82,7 +82,7 @@ Steffen Mueller, E<lt>currency-module at steffen-mueller dot netE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2003 by Steffen Mueller
+Copyright 2003-2005 by Steffen Mueller
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself. 
